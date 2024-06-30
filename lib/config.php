1cc1ee250a2d2aae88f82241ed623ca06db85a15
stephaneG-frep <119919153+stephaneG-frep@users.noreply.github.com>
2024-06-30 22:04:02 +0200

    suite

--- lib/config.php
+++ lib/config.php
@@ -1,3 +1,9 @@
 <?php
 
 define("_RECIPES_IMG_PATH_", "uploads/recipes/");
+
+$mainMenu = [
+    "index.php" => "Accueil",
+    "recettes.php" => "Vos recettes",
+    "a_propos.php" => "A-propos",
+];
